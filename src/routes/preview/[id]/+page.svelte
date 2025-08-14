55b7456fef5164403996a5fbfd8881162127ed1d
brysonjbest <103070659+brysonjbest@users.noreply.github.com>
2025-08-14 15:54:04 -0700

    Add additional param routing for previews.

--- src/routes/preview/[id]/+page.svelte
+++ src/routes/preview/[id]/+page.svelte
@@ -2,7 +2,12 @@
 	import ExternalPreviewPage from '../ExternalPreviewPage.svelte';
 	import { page } from '$app/state';
 
-	let id = $state(page.params.id ?? '');
+	let id = $state(
+		page.params.id ??
+			page.url?.searchParams?.get('id') ??
+			page.url?.pathname?.split('/').filter(Boolean).pop() ??
+			''
+	);
 </script>
 
 <ExternalPreviewPage {id} />
