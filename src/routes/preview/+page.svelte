044243cf59a21238600547fa561e151687a1cb97
dbellbro <David.Bell-Brown@gov.bc.ca>
2026-01-29 09:54:40 -0800

    Read barcode from form_definition (#126)
    * move barcode to inside form_definition
    
    * fix missed barcode change in preview/id

--- src/routes/preview/+page.svelte
+++ src/routes/preview/+page.svelte
@@ -13,7 +13,6 @@
 	let error = $state('');
 	let saveData = $state<{ data: any } | undefined>(undefined);
 	let isLoading = $state(false);
-	let barcode = $state<{content: string} | undefined>(undefined);
 
 	const trustedOrigins = [
 		import.meta.env.VITE_TEMPLATE_REPO_URL,
@@ -108,7 +107,6 @@
 
 			jsonContent = boundData.form_definition || {};
 			saveData = boundData.data ? { data: boundData.data } : undefined;
-			barcode = boundData.metadata?.barcode || undefined;
 			present = true;
 		} catch (e) {
 			error = e instanceof Error ? e.message : 'Invalid JSON format or API error. Please correct it.';
@@ -131,7 +129,6 @@
 	<RenderFrame
 		formData={jsonContent}
 		{saveData}
-		{barcode}
 		mode={FORM_MODE.preview}
 		formDelivery={isPortalIntegrated ? FORM_DELIVERY_MODE.portal : undefined}
 		goBack={handleGoBack}
